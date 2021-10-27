c4f068e075ded4b4690bf48d989ce963d4b71ffc
aicul313 <93240906+aicul313@users.noreply.github.com>
2021-10-27 14:45:17 +0900

    별 찍기 - 3

---
+++ 백준 2440번.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner n = new Scanner(System.in);
+        int userInputNumber = n.nextInt();
+
+        int lineCount = userInputNumber;
+        int starCount = userInputNumber;
+
+        for(int i=0; i<lineCount; i++){
+            for(int j=0; j<starCount; j++){
+                System.out.print("*");
+            }
+
+            starCount -= 1;
+
+            System.out.println();
+        }
+
+    }
+}

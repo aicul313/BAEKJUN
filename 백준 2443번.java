e74b90ec0fa8027401f71cda656efa74f04f495b
aicul313 <93240906+aicul313@users.noreply.github.com>
2021-10-27 14:47:37 +0900

    별 찍기 - 6

---
+++ 백준 2443번.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner n = new Scanner(System.in);
+        int userInputNumber = n.nextInt();
+
+        int lineCount = userInputNumber;
+        int starCount = userInputNumber * 2 - 1;
+        int spaceCount = 0;
+
+        for(int i=0; i<lineCount; i++){
+            for(int j=0; j<spaceCount; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<starCount; j++){
+                System.out.print("*");
+            }
+     
+            spaceCount += 1;
+            starCount -= 2;
+
+            System.out.println();
+            
+        }
+
+    }
+}

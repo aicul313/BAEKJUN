2764956f98cf33c95599967f3266ba2d998bf7c1
현지 <93240906+aicul313@users.noreply.github.com>
2021-10-28 15:00:51 +0900

    We love kriii

---
+++ 백준 10718번.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        for(int i=0; i<2; i++){
+            System.out.print("강한친구 대한육군");
+            System.out.println();
+        }
+
+    }
+}

1f5dc72f30de54bfd86cd710c911d90699771e74
현지 <93240906+aicul313@users.noreply.github.com>
2021-10-28 15:38:56 +0900

    A+B

---
+++ 백준 1000번.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+
+        Scanner n = new Scanner(System.in);
+        int A = n.nextInt();
+        int B = n.nextInt();
+
+        int sum = A + B;
+
+        System.out.println(sum);
+
+    }
+}

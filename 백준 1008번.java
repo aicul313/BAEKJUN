31ec20de48507183073062efedf87d481ceb3b8e
현지 <93240906+aicul313@users.noreply.github.com>
2021-10-28 15:59:08 +0900

    A / B

---
+++ 백준 1008번.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+
+        Scanner n = new Scanner(System.in);
+        double A = n.nextDouble();
+        double B = n.nextDouble();
+
+        double sum = A / B;
+
+        System.out.println(sum);
+
+    }
+}

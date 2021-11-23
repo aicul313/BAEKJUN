84279bc6c808ca1576ecc7ce103db33e6f5afd82
현지 <93240906+aicul313@users.noreply.github.com>
2021-11-23 17:46:22 +0900

    아스키 코드

---
+++ 백준 11654번.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        
+        String A = n.next();
+        
+        System.out.println(A.charAt(0)+0);
+    
+        n.close();
+    }
+
+}

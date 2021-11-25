b74a35e97d534815c7bb05b5d623895a44d73c68
현지 <93240906+aicul313@users.noreply.github.com>
2021-11-25 19:13:28 +0900

    달팽이는 올라가고 싶다 - BufferedReader 사용

---
+++ 백준 2869번-retry.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        StringTokenizer st;
+
+        BufferedReader n = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(n.readLine()," ");
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = ( V - B ) / ( A - B );
+
+        if (( V - B ) % ( A - B ) != 0){
+            day++;
+        }
+
+        System.out.println(day);
+
+        n.close();
+
+    }
+}

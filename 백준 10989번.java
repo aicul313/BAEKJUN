77951933cd5211b9a2ea61c4bd509a35236d2b99
현지 <93240906+aicul313@users.noreply.github.com>
2021-11-27 15:23:34 +0900

    수 정렬하기 3

--- 백준 10989번.java
+++ 백준 10989번.java
@@ -5,10 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int arr[] = new int[N];
-
         StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<N; i++){

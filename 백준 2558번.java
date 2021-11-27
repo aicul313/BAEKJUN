b4bf3c4f3f79d063ee8bbde2c113c93bf0b18378
현지 <93240906+aicul313@users.noreply.github.com>
2021-11-27 15:35:33 +0900

    A + B - 2

---
+++ 백준 2558번.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        Scanner n = new Scanner(System.in);
+
+        int A = n.nextInt();
+        int B = n.nextInt();
+        n.close();
+
+        System.out.println(A+B);
+
+    }
+}

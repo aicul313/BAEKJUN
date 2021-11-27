03e94c4902b43856d5e7ce6b2cda272e57c35a31
현지 <93240906+aicul313@users.noreply.github.com>
2021-11-27 14:23:18 +0900

    더하기

---
+++ 백준 14918번.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(input[0]) + Integer.parseInt(input[1]));
+
+    }
+}

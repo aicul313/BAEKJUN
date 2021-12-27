40e161862095f5ef06d28b521c7c67f5dab97e04
현지 <93240906+aicul313@users.noreply.github.com>
2021-12-27 15:36:26 +0900

    마이크로소프트 로고

---
+++ 백준 5338번.java
@@ -0,0 +1,18 @@
+import java.util.*;
+//import java.io.*;
+
+public class Main{
+
+    public static void main(String[] args) {
+    
+        String logo = "       _.-;;-._" + "\n"
+					+ "'-..-'|   ||   |" + "\n"
+					+ "'-..-'|_.-;;-._|" + "\n"
+					+ "'-..-'|   ||   |" + "\n"
+					+ "'-..-'|_.-''-._|";
+		
+		System.out.println(logo);
+
+    }
+
+}

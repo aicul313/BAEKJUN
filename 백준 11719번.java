6ef9673b4dd27dda0c71d16228a48bc44d292dd8
현지 <93240906+aicul313@users.noreply.github.com>
2021-12-31 18:05:38 +0900

    그대로 출력하기 2

---
+++ 백준 11719번.java
@@ -0,0 +1,18 @@
+import java.util.*;
+//import java.io.*;
+
+public class Main{
+
+    public static void main(String[] args) {
+    
+		Scanner sc= new Scanner(System.in);
+
+		while(sc.hasNextLine()){
+			System.out.println(sc.nextLine());
+		}
+
+		sc.close();
+
+    }
+
+}

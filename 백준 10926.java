4fd725d9e1ba9ef9b935e8ba069fcdc34fe1d37b
현지 <93240906+aicul313@users.noreply.github.com>
2023-04-05 18:32:32 +0900

    ??!

---
+++ 백준 10926.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+         
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = br.readLine();
+        String last = new String("??!");
+
+        System.out.println(A+last);
+
+    }
+}

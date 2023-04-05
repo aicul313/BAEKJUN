af0335bd64caaa2c5230ef06b932c5a57919208e
현지 <93240906+aicul313@users.noreply.github.com>
2023-04-05 19:43:52 +0900

    꼬마정민

---
+++ 백준 11382번-5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long answer = A+B+C;
+
+        System.out.println(answer);
+
+    }
+}

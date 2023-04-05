9eeb586f17d4c4cc9589c6bb19379bc6dce6276b
현지 <93240906+aicul313@users.noreply.github.com>
2023-04-05 19:43:14 +0900

    꼬마정민 (런타임 에러)

---
+++ 백준 11382번-4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        StringTokenizer st;
+
+        Scanner n = new Scanner(System.in);
+        st = new StringTokenizer(n.nextLine(), " ");
+        int answer = 0;
+
+        n.close();
+
+        while(st.hasMoreTokens()){
+            int a = Integer.parseInt(st.nextToken());
+            answer = answer + a;
+        }
+
+        System.out.println(answer);
+
+    }
+}
